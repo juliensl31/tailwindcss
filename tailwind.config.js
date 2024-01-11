657af8f56a76a0553c0ed0e6558747ae138e606e
juliensl31 <47530405+juliensl31@users.noreply.github.com>
2024-01-11 12:53:43 +0100

    personnalisation tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ['./src/**/*{html,js}'];
+export const content = ["./src/**/*{html,js}"];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: {
+        claire: "#fff3e5",
+        DEFAULT: "#ffd6a5",
+        sombre: "#b36100",
+        100: "#ffc580",
+      },
+    },
+  },
 };
 export const plugins = [];
